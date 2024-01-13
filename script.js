fdfe04c0585f3a543d6fb2b27eed72416c359a1b
SebastianMT7 <STorney@web.de>
2024-01-13 17:19:54 +0100

    progress-bar,audio,fonts

--- script.js
+++ script.js
@@ -41,8 +41,10 @@ let questions = [
     },
 ];
 let rightAnswer = 0;
-
 let currentQuestion = 0;
+let audio_right = new Audio('audio/right.wav');
+let audio_wrong = new Audio('audio/wrong.wav');
+
 
 function init() {
     document.getElementById('all-questions').innerHTML = questions.length;
@@ -50,36 +52,56 @@ function init() {
 }
 
 function showCurrentQuestion() {
-
-    if (currentQuestion >= questions.length) {
-        //End Screen(muss noch gemacht werden!)
-        document.getElementById('endScreen').style = '';
-        document.getElementById('questionBody').style = 'display: none';
-        document.getElementById('right-answers').innerHTML = rightAnswer;
-        document.getElementById('all-questions-endCard').innerHTML = questions.length;
+    if (gameIsOver()) {
+        showEndScreen();
     } else {
-        let question = questions[currentQuestion];
-        document.getElementById('current-question').innerHTML = currentQuestion + 1;
-        document.getElementById('questionText').innerHTML = question['question'];
-        document.getElementById('answer_1').innerHTML = question['answer_1'];
-        document.getElementById('answer_2').innerHTML = question['answer_2'];
-        document.getElementById('answer_3').innerHTML = question['answer_3'];
-        document.getElementById('answer_4').innerHTML = question['answer_4'];
+        updateprogressBar();
+        showQuestion();
     }
+}
+
+function gameIsOver() {
+    return currentQuestion >= questions.length;
+}
+
+function showEndScreen() {
+    document.getElementById('endScreen').style = '';
+    document.getElementById('questionBody').style = 'display: none';
+    document.getElementById('right-answers').innerHTML = rightAnswer;
+    document.getElementById('all-questions-endCard').innerHTML = questions.length;
+}
+
+function updateprogressBar() {
+    let percent = (currentQuestion + 1) / questions.length;
+    percent = Math.round(percent * 100); //rundet das Ergebnis
+    document.getElementById('progress-bar').innerHTML = `${percent}%`;
+    document.getElementById('progress-bar').style = `width:${percent}%;`;
+}
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+    document.getElementById('current-question').innerHTML = currentQuestion + 1;
+    document.getElementById('questionText').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
 }
 
 function answer(selection) {
     let question = questions[currentQuestion];
-    let selectetQuestionNumber = selection.slice(-1); //nimmt den letzten Buchstaben aus dem string(von der var answer_3 die 3. answer_3 wird durch onclick mit gegeben!!)
+    let selectedQuestionNumber = selection.slice(-1); //nimmt den letzten Buchstaben aus dem string(von der var answer_3 die 3. answer_3 wird durch onclick mit gegeben!!)
 
     let idOfRightAnswer = `answer_${question['right_answer']}`;
 
-    if (selectetQuestionNumber == question['right_answer']) {
+    if (selectedQuestionNumber == question['right_answer']) {
         document.getElementById(selection).parentNode.classList.add('bg-success');//mit .parentNode wählt man das übergeordnete Element aus (zB die div in dem nav tag)
-        rightAnswer ++;
+        audio_right.play();
+        rightAnswer++;
     } else {
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        audio_wrong.play();
     }
     document.getElementById('next-button').disabled = false;
 }
@@ -89,7 +111,6 @@ function nextQuestion() {
     document.getElementById('next-button').disabled = true;
     resetAnswerButtons();
     showCurrentQuestion();
-
 }
 
 function resetAnswerButtons() {
@@ -101,4 +122,12 @@ function resetAnswerButtons() {
     document.getElementById('answer_3').parentNode.classList.remove('bg-success');
     document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
     document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
+
+function restartGame() {
+    document.getElementById('questionBody').style = ''; //fragen container wieder anzeigen
+    document.getElementById('endScreen').style = 'display: none'; //§End Screen ausblenden
+    currentQuestion = 0;
+    rightAnswer = 0;
+    init();
 }
